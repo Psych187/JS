72786b5cf5f6cc29898abb6dbf8ff8aa0b29907f
Irina Streltsova <bananaboss3000@gmail.com>
2024-09-10 13:57:50 +0300

    seminar 2 added

---
+++ practice/2/script.js
@@ -0,0 +1,56 @@
+// let age = Number(prompt("How old are you"));
+// console.log(`You are ${age} YO`);
+// let userNAme = prompt("What's your name?");
+// console.log(`Welcome to site, ${userNAme}`);
+
+// let a = 15
+// console.log(b);
+
+// let a = '2';
+// let b = '3';
+// let a = Number(a);
+// let b = Number(b);
+// console.log(a+b, a*b);
+
+// let first = Number(prompt('Enter first num: '));
+// let second = Number(prompt('Enter second num: '));
+// console.log(`Sum of numbers is ${first + second}`);
+// console.log(`Diff of numbers is ${first - second}`);
+// console.log(`Multiple of numbers is ${first * second}`);
+// console.log(`Division of numbers is ${first / second}`);
+// console.log(`Reminder of numbers is ${first % second}`);
+
+// let userNum = Number(prompt('Enter your num: '));
+// if (userNum > 5) {
+//     alert('Number is bigger than 5');
+// } else if(userNum < 5) {
+//     alert('Number is less than 5');
+// } else {
+//     alert("Number is equal 5");
+// }
+
+// let test1 = Number(prompt());
+// let test2 = Number(prompt());
+// if (test1 === test2) {
+//     alert('equal');
+// } else if (test1 > test2) {
+//     alert('test1 > test2');
+// } else {
+//     alert('test1 < test2');
+// }
+
+// let first = Number(prompt('Enter first num: '));
+// let second = Number(prompt('Enter second num: '));
+// if (first < second) {
+//     console.log('First is minimal');
+
+// } else {
+//     console.log('Second is minimal');
+// }
+
+let num = Number(prompt());
+if (num > 0 && num < 15) {
+    alert("Numbers 0 to 15");
+} else {
+    console.log("Other numbers");
+}
